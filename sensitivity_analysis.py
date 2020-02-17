23682c83d80a76061bb2b0eec67f925b455fd9fa
Smiti Mittal <59079754+smitimittal@users.noreply.github.com>
2020-02-17 14:54:56 -0800

    Update sensitivity_analysis.py

--- sensitivity_analysis.py
+++ sensitivity_analysis.py
@@ -1,7 +1,8 @@
 import pandas as pd
 def analysis(processgrp, metric):
-	#find all processes within processgrp using quant_metrics, define listofprocesses
-	#draw values from normal distribution of each process in listofprocesses
-	#calculate mean of listofprocesses and store in listofmeans
-	#calculate SD of listofmeans
+	# repeat 10000 times:
+		#find all processes within processgrp, define listofprocesses
+		#draw values from normal distribution of each process in listofprocesses, store in tempvalues
+		#calculate mean of listofprocesses and store in listofmeans
+		#calculate SD of listofmeans
 	pass
