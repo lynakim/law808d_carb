53556f8cec88f25e9ac48a8df1256a7ebce63167
JiHunWang <46038281+JiHunWang@users.noreply.github.com>
2020-02-27 22:35:47 -0800

    Update process_metric_matching.py

--- process_metric_matching.py
+++ process_metric_matching.py
@@ -1,1 +1,16 @@
+import pandas as pd
 
+def import_csv():  
+    df = import_csv("quant_metrics.csv")
+    index = list(set(df['ProcessGroup']))
+    columns = list(set(df['Metric']))
+    table = pd.DataFrame(index=index, columns=columns)
+
+    for _, row in df.iterrows():
+        value = row["Value"]
+        stddev = row["StdDev"]
+        if stddev == 0:
+            stddev = value * 0.05
+        table.loc[row["ProcessGroup"], row["Metric"]] = (value, stddev)
+    
+    return table
